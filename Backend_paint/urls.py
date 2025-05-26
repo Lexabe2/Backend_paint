f3001eb471d0a428be4f97be5075a81956c0f943
Alekseu <natash79@inbox.ru>
2025-05-26 11:20:28 +0300

    add lambda

--- Backend_paint/urls.py
+++ Backend_paint/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 urlpatterns = [
+    path('', lambda request: HttpResponse("Сервер работает!")),
     path('admin/', admin.site.urls),
     path('api/', include('Backend_paint_app.urls')),  # Подключаем API один раз
 ]
