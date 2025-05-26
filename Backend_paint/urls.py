7ffb6ea6e7ab9450fcfaeddb7dd5ef6e8ea97c7e
Alekseu <natash79@inbox.ru>
2025-05-26 11:27:18 +0300

    изменил urls

--- Backend_paint/urls.py
+++ Backend_paint/urls.py
@@ -5,8 +5,9 @@ from django.conf.urls.static import static
 from django.http import HttpResponse
 
 urlpatterns = [
-    path('', lambda request: HttpResponse("Сервер работает!")),
     path('admin/', admin.site.urls),
-    path('api/', include('Backend_paint_app.urls')),  # Подключаем API один раз
+    path('api/', include('Backend_paint_app.urls')),  # Подключаем API
+    path('', lambda request: HttpResponse("Сервер работает!")),  # Заглушка для корня — последней
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
