3a1829820e1d64fea022e0a0054a957b32f87cac
aleksej <natash79@inbox.ru>
2025-06-16 11:18:17 +0300

    Добавил редактирование банкомата

--- Backend_paint_app/views.py
+++ Backend_paint_app/views.py
@@ -75,7 +75,7 @@ class LoginStep1View(APIView):
                             'parse_mode': 'Markdown'
                         }
                     )
-                    # ntn
+                    # nt
                     response.raise_for_status()
                     log_request_info(logger, request, 'Код отправлен в Telegram', level='info')
                 except requests.RequestException as e:
