c0cb3c4801586234b33b6cffdb374e4d9f4a7bc1
aleksej <natash79@inbox.ru>
2025-10-23 10:41:51 +0300

    Добавил приемку

--- Backend_paint_app/funk.py
+++ Backend_paint_app/funk.py
@@ -14,9 +14,20 @@ def changes_req(req, status, request):
     return True
 
 
-def changes_req_atm_funk(req, sn, request):
-    StatusATM.objects.create(status=f'Добавлен к заявке {req}', date_change=date.today(), user=request.user,
-                             sn=ATM.objects.get(serial_number=sn))
-    ATM.objects.filter(serial_number=Request.objects.get(request_id=sn)).update(
-        request=Request.objects.get(request_id=req), status=Request.objects.get(request_id=req).status)
+def changes_req_atm_funk(req_id, sn, request):
+    atm = ATM.objects.get(serial_number=sn)
+    req = Request.objects.get(request_id=req_id)
+
+    StatusATM.objects.create(
+        status=f'Добавлен к заявке {req_id}',
+        date_change=date.today(),
+        user=request.user,
+        sn=atm
+    )
+
+    ATM.objects.filter(serial_number=sn).update(
+        request=req,
+        status=req.status
+    )
+
     return True
